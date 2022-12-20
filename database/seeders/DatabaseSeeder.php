2ae29942e8f29f3a8bde641b7cf1da7715453c28
Umang Shrestha <sumang20@tbc.edu.np>
2022-12-20 13:45:09 +0545

    seeded for Role and ProductType

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\ProductType;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        ProductType::create([
+            'name' => 'Book'
+        ]);
+        ProductType::create([
+            'name' => 'CD'
+        ]);
+        ProductType::create([
+            'name' => 'Game'
+        ]);
+
+        Role::create([
+            'name' => 'Vendor'
+        ]);
+        Role::create([
+            'name' => 'Admin'
+        ]);
     }
 }
