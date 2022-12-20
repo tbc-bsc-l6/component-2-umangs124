4444cea2e68f66ca493dfda7d00bac6f42f0c91a
Umang Shrestha <sumang20@tbc.edu.np>
2022-12-20 13:38:45 +0545

    modified column by add role id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('role_id')->constrained()
+                ->onDelete('cascade');
         });
     }
 
