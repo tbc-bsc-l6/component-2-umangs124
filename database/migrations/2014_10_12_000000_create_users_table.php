cde769d6550da6e8448a6d216bb7e2a133369b59
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-05 21:36:41 +0545

    modified table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('verification_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->foreignId('role_id')->constrained()
