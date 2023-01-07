6c25305d9c2c10f0cfebb304fc69e48c39dafac3
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-07 10:56:31 +0545

    added interface

---
+++ app/Interfaces/ProductRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Interfaces;
+
+interface ProductRepositoryInterface 
+{
+    public function getAllProducts();
+    public function getAllProductTypes();
+    public function addProducts(array $formFields);
+    public function addProductHistory($productHistory);
+    public function getProductTypeNameById($productType);
+    public function getProductsByUserId($userId);
+    public function getProductByProductId($productId);
+    public function updateProduct($productId, $formFields);
+    public function deleteProduct($productId);
+}
---
+++ app/Interfaces/UserRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Interfaces;
+
+interface UserRepositoryInterface 
+{
+    public function getAllUsers();
+    public function getRoleIdWithIdOne();
+    public function addUser($user);
+    public function getUserById($id);
+    public function updateUser($userId, $formFields);
+    public function updatePassword($userId, $formFields);
+    public function updateVerificationCode($userId, $formFields);
+    public function deleteUser($userId);
+}
