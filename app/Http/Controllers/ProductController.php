7f5e125b258ce53936237a78a76d60af803958c0
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-07 10:52:53 +0545

    updated logic by using Product repository to communicate with database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,32 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\ProductHistory;
+use App\Interfaces\ProductRepositoryInterface;
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
+
 
 class ProductController extends Controller
 {
+    private ProductRepositoryInterface $productRepository;
 
+    public function __construct()
+    {
+        $this->productRepository = new ProductRepository();
+    }
     public function index()
     {
-        //
-        return view('products.index', ['products' => Product::all()]);
+        $products = $this->productRepository->getAllProducts();
+        return view('products.index', ['products' => $products]);
     }
-
     public function create()
     {
-        $productTypes = DB::table('product_types')->get();
+        $productTypes = $this->productRepository->getAllProductTypes();
         return view('products.create', ['productTypes' => $productTypes]);
     }
-
-
     public function store(Request $request)
     {
-
-        //dd($request);
         $formFields = $request->validate([
             'name' => 'required|min:3',
             'price' => 'required|numeric|between:0,99999999.999',
@@ -38,44 +38,36 @@ class ProductController extends Controller
         }
         $formFields['product_type_id'] = $request->productType;
         $formFields['user_id'] = $request->userId;
-        $product = Product::create($formFields);
 
-        $productTypeName = DB::table('product_types')->select('name')->where('id', '=', $request->productType)->first();
+        $productId = $this->productRepository->addProducts($formFields);
 
-        // dd($productTypeName);
+        $product = $this->productRepository->getProductByProductId($productId);
 
+        $productTypeName = $this->productRepository->getProductTypeNameById($request->productType);
         $productHistory['product_name'] = $product->name;
         $productHistory['product_price'] = $product->price;
         $productHistory['product_type'] = $productTypeName->name;
         $productHistory['user_id'] = $request->userId;
         $productHistory['action'] = "Product Added";
-        ProductHistory::create($productHistory);
+        $this->productRepository->addProductHistory($productHistory);
         return redirect('showProductByVendorId')->with('message', 'Product added successfully');
     }
 
 
     public function show()
     {
-        $products = Product::query('products')
-            ->join('product_types', 'products.product_type_id', '=', 'product_types.id')
-            ->select('products.*', 'product_types.name as productType')
-            ->where('user_id', '=', Auth::user()?->id)
-            ->filter(request(['product_search']))->paginate(4);
-        //   dd($products);
+        $products = $this->productRepository->getProductsByUserId(Auth::user()?->id);
         return view('products.show', ['products' => $products]);
     }
 
-
     public function edit($id)
     {
-        $product = DB::table('products')->where('id', '=', $id)->first();
+        $product = $this->productRepository->getProductByProductId($id);
         return view('products.edit', ['product' => $product]);
     }
 
     public function update(Request $request)
     {
-        
-        $product = Product::find($request->productId);
         $formFields = $request->validate([
             'price' => 'required|numeric|between:0,99999999.999',
             'description' => 'required|min:8'
@@ -83,32 +75,25 @@ class ProductController extends Controller
         if ($request->hasFile('image')) {
             $formFields['image'] = $request->file('image')->store('productImage', 'public');
         }
+        $product = $this->productRepository->getProductByProductId($request->productId);
+        $this->productRepository->updateProduct($request->productId, $formFields);
 
-        $product->update($formFields);
-
-        $productTypeName = DB::table('product_types')->select('name')->where('id', '=', $request->productType)->first();        
+        $productTypeName = $this->productRepository->getProductTypeNameById($request->productType);
 
         $productHistory['product_name'] = $product->name;
         $productHistory['product_price'] = $request->price;
         $productHistory['product_type'] = $productTypeName->name;
         $productHistory['user_id'] = $request->userId;
         $productHistory['action'] = "Product Updated";
-        ProductHistory::create($productHistory);
+
+        $this->productRepository->addProductHistory($productHistory);
+
         return redirect('showProductByVendorId')->with('message', 'Product updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $product = Product::find($id);
-        $product->delete();
-
+        $this->productRepository->deleteProduct($id);
         return redirect('showProductByVendorId')->with('message', 'Product deleted successfully');
     }
-
 }
