b580b6faf622e0d7eb45a26f04367bc7aace9bdd
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-07 10:53:38 +0545

    updated logic by using User repository to communicate with database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
+use App\Repositories\UserRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -11,41 +14,26 @@ use Illuminate\Support\Facades\Session;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private UserRepositoryInterface $userRepository;
+    public function __construct()
+    {
+        $this->userRepository = new UserRepository();
+    }
     public function index()
     {
-        $users = User::query('users')->where('role_id', '=', 1)->filter(request(['user_search']))->paginate(4);
+        $users = $this->userRepository->getAllUsers();
         return view('vendors.index', ['users' => $users]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        // if (Cache::has("getRoleIdOne")) {
-        //     $roleId = Cache::get(("getRoleIdOne"));
-        //     return view('admin.create', ['roleId' => $roleId]);
-        // }
-        $roleId = DB::table('roles')->select('id')->where('id', '=', 1)->get();
-        // Cache::put("getRoleIdOne", $roleId, now()->addminutes(10));
+        $roleId = $this->userRepository->getRoleIdWithIdOne();
         return view('vendors.create', ['roleId' => $roleId]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        
         $formFields = $request->validate([
             'name' => 'required|min:3',
             'email' => 'required|email|unique:users',
@@ -55,39 +43,23 @@ class UserController extends Controller
             $formFields['image'] = $request->file('image')->store('userImage', 'public');
         }
         $roleId = json_decode($request->roleId);
-
         $formFields['role_id'] = $roleId[0]->id;
 
         $formFields['password'] = bcrypt($formFields['password']);
-
-        User::create($formFields);
+        $this->userRepository->addUser($formFields);
 
         return redirect('showAllVendors')->with('message', 'User created successfully');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        $user = DB::table('users')->where('id', '=', $id)->first();
+        $user = $this->userRepository->getUserById($id);
         return view('vendors.edit', ['user' => $user]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request)
     {
-        $user = User::find($request->id);
-
+        
         $formFields = $request->validate([
             'name' => 'required',
             'email' => 'required|email'
@@ -95,21 +67,15 @@ class UserController extends Controller
         if ($request->hasFile('image')) {
             $formFields['image'] = $request->file('image')->store('userImage', 'public');
         }
-        $formFields['role_id'] = $request->roleId;
-        $user->update($formFields);
+      
+        $this->userRepository->updateUser($request->userId, $formFields);
+        
         return redirect('showAllVendors')->with('message', 'User created successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $user = User::find($id);
-        $user->delete();
+        $this->userRepository->deleteUser($id);
         return redirect('showAllVendors')->with('message', 'User deleted successfully');
     }
 
@@ -117,21 +83,10 @@ class UserController extends Controller
     {
         return view('vendors.changePasswordForm');
     }
-    public function changePassword(Request $request)
-    {
-        $user = User::find(Auth::user()?->id);
-        $formFields = $request->validate([
-            'password' => 'required|confirmed|min:6',
-        ]);
-        $formFields['password'] = bcrypt($formFields['password']);
-        $user->update($formFields);
-        return redirect('showProductByVendorId')->with('message', 'Password changed succesfully');
-    }
 
     public function sendVerificationCode(Request $request)
     {
         $code = random_int(100000, 999999);
-
         $data = ['code' => $code];
         $user['email'] = Auth::user()?->email;
         Mail::send('mail', $data, function ($messages) use ($user) {
@@ -140,8 +95,8 @@ class UserController extends Controller
         });
 
         $formFields['verification_token'] = $code;
-        $user = User::find(Auth::user()?->id);
-        $user->update($formFields);
+
+        $this->userRepository->updateVerificationCode(Auth::user()?->id, $formFields);
         Session::put('changedPassword', $request->password);
         return redirect('verificationCodeForm')->with('message', 'Please Enter Verification Code');
     }
@@ -150,7 +105,7 @@ class UserController extends Controller
     {
         return view('vendors.verificationCodeForm');
     }
-    
+
     public function verifyVerificationCode(Request $request)
     {
         $getCode = $request->code;
@@ -159,10 +114,9 @@ class UserController extends Controller
         if ($getCode != $actualCode->verification_token) {
             return back()->withErrors(['verification_token' => 'Invalid Token']);
         }
-        $user = User::find(Auth::user()?->id);
         $formFields['password'] = bcrypt(Session::get('changedPassword'));
         $formFields['verification_token'] = null;
-        $user->update($formFields);
+        $this->userRepository->updatePassword(Auth::user()?->id, $formFields);
         return redirect('showProductByVendorId')->with('message', 'Password Changed Successfully');
     }
 }
