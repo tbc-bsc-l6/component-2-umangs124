e00bba25fd3cb9da86d451445d8616ac69030442
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-09 00:09:30 +0545

    added name in fillable

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProductType extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
     // Relationship to Product
     public function products()
     {
