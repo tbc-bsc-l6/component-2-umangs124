865f30c25a7661d36707eff1bf68ff8309740ed5
Umang Shrestha <sumang20@tbc.edu.np>
2023-01-09 00:10:43 +0545

    added few more routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductHistoryController;
+use App\Http\Controllers\ProductTypeController;
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,30 +25,38 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 // show All users for admin
-Route::get('showAllVendors', [UserController::class, 'index'])->middleware('auth');
+Route::get('showAllVendors', [UserController::class, 'index'])->middleware('admin');
 
 // Show create form for users
-Route::get('showCreateVendorForm', [UserController::class, 'create'])->middleware('auth');
+Route::get('showCreateVendorForm', [UserController::class, 'create'])->middleware('admin');
 
 // Store user data by admin
-Route::post('createVendor', [UserController::class, 'store'])->middleware('auth');
+Route::post('createVendor', [UserController::class, 'store'])->middleware('admin');
 
 // show edit form for users for admin
-Route::get('showEditVendorForm/{vendor}', [UserController::class, 'edit'])->middleware('auth');
+Route::get('showEditVendorForm/{vendor}', [UserController::class, 'edit'])->middleware('admin');
 
 // update user by admin
-Route::put('updateVendor/{vendors}', [UserController::class, 'update'])->middleware('auth');
+Route::put('updateVendor/{vendors}', [UserController::class, 'update'])->middleware('admin');
 
 // delete user by admin
-Route::delete('deleteVendor/{vendors}', [UserController::class, 'destroy'])->middleware('auth');
+Route::delete('deleteVendor/{vendors}', [UserController::class, 'destroy'])->middleware('admin');
 
-Route::get('showChangePasswordForm', [UserController::class, 'showChangePasswordForm'])->middleware('auth');
+Route::get('showChangePasswordForm', [UserController::class, 'showChangePasswordForm'])->middleware('vendor');
 Route::post('changePassword', [UserController::class, 'changePassword']);
-Route::post('sendVerificationCode', [UserController::class, 'sendVerificationCode'])->middleware('auth');
+Route::post('sendVerificationCode', [UserController::class, 'sendVerificationCode'])->middleware('vendor');
 
-Route::get('verificationCodeForm', [UserController::class, 'verificationCodeForm'])->middleware('auth');
-Route::post('verifyVerificationCode', [UserController::class, 'verifyVerificationCode'])->middleware('auth');
+Route::get('verificationCodeForm', [UserController::class, 'verificationCodeForm'])->middleware('vendor');
+Route::post('verifyVerificationCode', [UserController::class, 'verifyVerificationCode'])->middleware('vendor');
 
+Route::get('showProductHistories', [ProductHistoryController::class, 'index'])->middleware('admin');
+Route::delete('deleteProductHistories/{productHistories}', [ProductHistoryController::class, 'destroy'])->middleware('admin');
+
+// Product Type
+Route::get('showProductTypes', [ProductTypeController::class, 'index']);
+Route::get('productTypeCreateForm', [ProductTypeController::class, 'create']);
+Route::post('addProductType', [ProductTypeController::class, 'store']);
+Route::delete('deleteProductType/{productType}', [ProductTypeController::class, 'destroy']);
 
 /*
 |--------------------------------------------------------------------------
@@ -64,10 +75,10 @@ Route::post('users/logout', [AuthController::class, 'logout'])->middleware('auth
 | Routes for Products
 |--------------------------------------------------------------------------
 */
-Route::get('showCreateProductForm', [ProductController::class, 'create'])->middleware('auth');
+Route::get('showCreateProductForm', [ProductController::class, 'create'])->middleware('vendor');
 Route::get('/', [ProductController::class, 'index']);
-Route::get('showProductByVendorId', [ProductController::class, 'show'])->middleware('auth');
-Route::get('showEditProductForm/{product}', [ProductController::class, 'edit'])->middleware('auth');
-Route::put('updateProduct/{product}', [ProductController::class, 'update'])->middleware('auth');
-Route::post('createProduct', [ProductController::class, 'store'])->middleware('auth');
-Route::delete('deleteProduct/{products}', [ProductController::class, 'destroy'])->middleware('auth');
+Route::get('showProductByVendorId/{vendors}', [ProductController::class, 'show'])->middleware('auth');
+Route::get('showEditProductForm/{product}', [ProductController::class, 'edit'])->middleware('vendor');
+Route::put('updateProduct/{product}', [ProductController::class, 'update'])->middleware('vendor');
+Route::post('createProduct', [ProductController::class, 'store'])->middleware('vendor');
+Route::delete('deleteProduct/{products}', [ProductController::class, 'destroy'])->middleware('vendor');
